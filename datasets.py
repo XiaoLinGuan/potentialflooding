c428fbc6c80c7bf8deda7f69234d4087435ae28c
XiaoLiiinnn <44045798+XiaoLinGuan@users.noreply.github.com>
2021-12-01 20:19:47 -0500

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,26 @@
+"""
+Filter and analysis of datasets
+"""
+
+import pandas as pd
+# import numpy as np
+
+
+# Datasets for Map
+
+
+# Datasets for Stacked Barchart
+
+
+# Datasets for Pie Chart/Donut Chart
+
+
+# Datasets for Line Graph
+global_sl = pd.read_csv('global_sea_level.csv')
+
+
+# Datasets for Scatter Plot
+cyclones_AO = pd.read_csv('cyclones_AO.csv')
+landfall_EC = pd.read_csv('cyclones_landfall_EC.csv')
+tri_state_and_nyc = pd.read_csv('cyclones_tri_state_and_nyc.csv')
+
