619e210d6b43abac882abb9458516d60337f50bd
XiaoLiiinnn <44045798+XiaoLinGuan@users.noreply.github.com>
2021-12-01 23:37:09 -0500

    Add files via upload

--- datasets.py
+++ datasets.py
@@ -17,10 +17,11 @@ import pandas as pd
 
 # Datasets for Line Graph
 global_sl = pd.read_csv('global_sea_level.csv')
-
+ec_sl = pd.read_csv('ec_sea_level.csv')
+nyc_sl = pd.read_csv('nyc_sea_level.csv')
+all_sl = pd.read_csv('all_sea_level.csv')
 
 # Datasets for Scatter Plot
 cyclones_AO = pd.read_csv('cyclones_AO.csv')
 landfall_EC = pd.read_csv('cyclones_landfall_EC.csv')
 tri_state_and_nyc = pd.read_csv('cyclones_tri_state_and_nyc.csv')
-
