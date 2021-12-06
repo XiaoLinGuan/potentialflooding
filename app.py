cd11903448fe746b26d7e0eef5910656ef6994de
xlg2120 <44045798+XiaoLinGuan@users.noreply.github.com>
2021-12-06 14:37:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -530,7 +530,7 @@ def build_scatter_plot(value):
 		size = "Count",
 		trendline = "ols",
 		trendline_scope = "overall",
-		trendline_color_override = "#6C3483"
+		trendline_color_override = "#6C3483",
 		title = title
 	)
 	fig.update_layout(paper_bgcolor="#DDF2D1", plot_bgcolor="#BEE3BA")
